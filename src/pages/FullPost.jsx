2b224c6864395536dfe3ead608ef5790c1167661
Andrey Emelianov <621077@mail.ru>
2023-09-19 21:02:00 +0300

    добавил отображение загруженного изображения для статьи и в компоненте Home и в компоненте FullPost

--- src/pages/FullPost.jsx
+++ src/pages/FullPost.jsx
@@ -5,6 +5,7 @@ import { Post } from '../components/Post';
 import { Index } from '../components/AddComment';
 import { CommentsBlock } from '../components/CommentsBlock';
 import { useParams } from 'react-router-dom';
+import ReactMarkdown from 'react-markdown';
 
 export const FullPost = () => {
   const [data, setData] = React.useState();
@@ -34,14 +35,14 @@ export const FullPost = () => {
       <Post
         id={data._id}
         title={data.title}
-        imageUrl={data.imageUrl}
+        imageUrl={`http://localhost:4444${data.imageUrl}`}
         user={data.user}
         createdAt={data.createdAt}
         viewsCount={data.viewsCount}
         commentsCount={3}
         tags={data.tags}
         isFullPost>
-        <p>{data.text}</p>
+        <ReactMarkdown children={data.text} />
       </Post>
       <CommentsBlock
         items={[
