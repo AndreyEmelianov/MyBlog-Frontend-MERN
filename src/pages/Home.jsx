0a411747e16753931e453e30af66abc265189340
Andrey Emelianov <621077@mail.ru>
2023-09-19 17:40:21 +0300

    создал asyncThunk на получение постов с бэкенда и в extraReducers описал три состояния

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,19 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Grid from '@mui/material/Grid';
 
+import axios from '../axios';
 import { Post } from '../components/Post';
 import { TagsBlock } from '../components/TagsBlock';
 import { CommentsBlock } from '../components/CommentsBlock';
+import { useDispatch } from 'react-redux';
+import { fetchPosts } from '../redux/Slices/posts';
 
 export const Home = () => {
-  React.useEffect(() => {}, []);
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    dispatch(fetchPosts());
+  }, []);
 
   return (
     <>
